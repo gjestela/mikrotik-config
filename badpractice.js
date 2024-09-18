046cec90c4ebe50c375232a60528594470623eb6
Eirik Gjesteland <49169960+gjestela@users.noreply.github.com>
2024-09-18 11:57:21 +0200

    Update badpractice.js

--- badpractice.js
+++ badpractice.js
@@ -3,5 +3,7 @@ let username = 'username';
 let password = 'mySecretPassword';
 
 let sql = "SELECT * FROM TABLE WHERE id=" + window.location.search;
+
+runSql('10.0.20.20', username, password, sql);
   
 </script>
